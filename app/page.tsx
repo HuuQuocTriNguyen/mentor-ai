3051092b08e01825898b6f14f891f7aecf0fe1eb
Christopher nguyen <chrisnguyen1304@Christophers-MacBook-Pro.local>
2025-07-04 15:36:21 -0600

    Build a companion + dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+      <h1 className='text-2xl'>Dashboard</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+          name='Neura the Brainy Explorer'
+          topic='Neural Network of the Brain'
+          subject='Science'
+          duration={45}
+          color='#E5D0FF'
+        />
+        
+        <CompanionCard
+          id='456'
+          name='Countsy the Number Wizard'
+          topic='Derivatives & Integrals'
+          subject='Math'
+          duration={30}
+          color='#FFDA6E'
+        />
+
+        <CompanionCard
+          id='789'
+          name='Verba the Vocabulary Builder'
+          topic='English Literature '
+          subject='Language'
+          duration={30}
+          color='#BDE7FF'
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList 
+          title="Recently completed sessions"
+          companions={recentSessions}
+          className="w-2/3 max-lg:full" 
+        />
+        
+        <CTA />
+      </section>
+
+    </main>
   )
 }
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image"
 import NavItems from "./navItems"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav>
-      <link href="/">
+    <nav className="navbar">
+      <Link href="/">
         <div className="flex items-center gap-2.5 cursor pointer"> 
-          <Image src="/images/logo.png" alt="Converso Logo" width={46} height={44} />
+          <Image src="/images/logo.svg" alt="Converso Logo" width={46} height={44} />
         </div>
-      </link>
+      </Link>
 
       <div className="flex items-center gap-8">
         <NavItems />
